7dffbd5293d7e1ad85f3f5bfd9a47bce90d88c90
Oliver Dixon <od641@york.ac.uk>
2023-04-22 23:34:01 +0100

    Reverted the stack initialiser
    An initial allocation of some arbitrary number of nodes is done.

--- c/stack.c
+++ c/stack.c
@@ -74,12 +74,15 @@ struct stack * stack_initialise ( unsigned int capacity )
         if ( !capacity )
                 capacity = DEFAULT_CAPACITY;
 
-        if ( ( self = malloc ( sizeof ( struct stack ) ) ) ) {
-                self->capacity = 1;
-                self->size = 0;
-
-                debug_puts ( "Stack initialised" );
-        }
+        if ( ( self = malloc ( sizeof ( struct stack ) ) ) )
+                if ( ! ( self->data = malloc ( sizeof ( void * ) *
+                                capacity ) ) )
+                        free ( self );
+                else {
+                        self->capacity = capacity;
+                        self->size = 0;
+                        debug_puts ( "Stack initialised" );
+                }
 
         return self;
 }
